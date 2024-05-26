f1398d1665f9a42176f4e7704ddc3d58635f9f08
Lucas-Salomao <lucastadeusalomao@gmail.com>
2024-05-26 19:31:08 -0300

    first commit

---
+++ src/mail/types/message.ts
@@ -0,0 +1,11 @@
+
+type DataMessage = {
+    id: string;
+    idUser: string;
+    orderNumber: number;
+    orderValue: number;
+    paymentConfirmed: boolean;
+  };
+  
+  export default DataMessage;
+  
